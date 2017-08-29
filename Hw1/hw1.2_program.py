8097b705b121e7fbceb164d421d8bd89366aa77f
Jordan Giebas <jordangiebas@Jordans-MacBook-Pro.local>
2017-08-29 15:17:11 -0400

    Finished part e, d of Hw1 Question 2. Need to do the three-asset analogue, then finish question 3.

--- Hw1/hw1.2_program.py
+++ Hw1/hw1.2_program.py
@@ -15,27 +15,32 @@ Date:
 """
 
 # Used for plotting
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 #from itertools import accumulate ## Use this to get the sum of elements in an iterable
 from datetime import datetime
 import numpy as np
 from math import sqrt
 
+
 def SharpeRatio( asset_expReturn, asset_var, market_r_r ):
 
-    return (asset_expReturn - market_rates)/math.sqrt(asset_var)
+    return (asset_expReturn - market_rates)/sqrt(asset_var)
 
 def ExpRet_Var_and_Corr( l, bigl ):
 
-    curr_exp_return = 12*np.mean(l) + 0.01
-    variance = np.var(l)
+    ## Quantities of interest
+    curr_exp_return = round(float(12*np.mean(l) + 0.01), 3)
+    avg_monthly_variance = np.var(l)
+    annual_variance = round(float(12*avg_monthly_variance),3)
+    annual_std = sqrt(annual_variance)
+    sharpe_ratio = round((curr_exp_return - 0.01)/float(annual_std),3)
 
     corr_list = list()
     for elm in bigl:
 
-        corr_list.append( round(np.corrcoef(l, elm, ddof=0)[0][1], 3) )
+        corr_list.append(round(float(np.corrcoef(l, elm, ddof=0)[0][1]),3))
 
-    return [curr_exp_return, variance, corr_list]
+    return [sharpe_ratio, curr_exp_return, annual_variance, corr_list]
 
 ## Define containers for needed quantitites
 rf_rates = list()
@@ -81,59 +86,120 @@ assetList.append(rtail_rates)
 
 bigList = list()
 
+
+### Question 2: Parts a,b,c
+### Get needed quantities
+
+#print("\nSHARPE RATIOS\n")
 ## Market
-market_cer, market_var, market_corr = ExpRet_Var_and_Corr(market_rates, assetList)
+market_sr, market_cer, market_var, market_corr = ExpRet_Var_and_Corr(market_rates, assetList)
 bigList.append(ExpRet_Var_and_Corr(market_rates, assetList))
 
+#print( "Market: ", market_cer, ",", market_var, ",", market_sr )
+
 ## Aero
-aero_cer, aero_var, aero_corr = ExpRet_Var_and_Corr(aero_rates, assetList)
+aero_sr, aero_cer, aero_var, aero_corr = ExpRet_Var_and_Corr(aero_rates, assetList)
 bigList.append(ExpRet_Var_and_Corr(aero_rates, assetList))
 
+#print( "\nAero: ", aero_cer, ",", aero_var, ",", aero_sr )
+
+
 ## Guns
-guns_cer, guns_var, guns_corr = ExpRet_Var_and_Corr(guns_rates, assetList)
+guns_sr, guns_cer, guns_var, guns_corr = ExpRet_Var_and_Corr(guns_rates, assetList)
 bigList.append(ExpRet_Var_and_Corr(guns_rates, assetList))
 
+#print( "\nGuns: ", guns_cer, ",", guns_var, ",", guns_sr )
+
+
 ## Steel
-steel_cer, steel_var, steel_corr = ExpRet_Var_and_Corr(steel_rates, assetList)
+steel_sr, steel_cer, steel_var, steel_corr = ExpRet_Var_and_Corr(steel_rates, assetList)
 bigList.append(ExpRet_Var_and_Corr(steel_rates, assetList))
 
+#print( "\nSteel: ", steel_cer, ",", steel_var, ",", steel_sr )
+
 ## Ships
-ships_cer, ships_var, ships_corr = ExpRet_Var_and_Corr(ships_rates, assetList)
+ships_sr, ships_cer, ships_var, ships_corr = ExpRet_Var_and_Corr(ships_rates, assetList)
 bigList.append(ExpRet_Var_and_Corr(ships_rates, assetList))
 
+#print( "\nShips: ", ships_cer, ",", ships_var, ",", ships_sr )
+
 ## Beer
-beer_cer, beer_var, beer_corr = ExpRet_Var_and_Corr(beer_rates, assetList)
+beer_sr, beer_cer, beer_var, beer_corr = ExpRet_Var_and_Corr(beer_rates, assetList)
 bigList.append(ExpRet_Var_and_Corr(beer_rates, assetList))
 
+#print( "\nBeer: ", beer_cer, ",", beer_var, ",", beer_sr )
+
 ## Toys
-toys_cer, toys_var, toys_corr = ExpRet_Var_and_Corr(toys_rates, assetList)
+toys_sr, toys_cer, toys_var, toys_corr = ExpRet_Var_and_Corr(toys_rates, assetList)
 bigList.append(ExpRet_Var_and_Corr(toys_rates, assetList))
 
+#print( "\nToys: ", toys_cer, ",", toys_var, ",", toys_sr )
+
 ## Fin
-fin_cer, fin_var, fin_corr = ExpRet_Var_and_Corr(fin_rates, assetList)
+fin_sr, fin_cer, fin_var, fin_corr = ExpRet_Var_and_Corr(fin_rates, assetList)
 bigList.append(ExpRet_Var_and_Corr(fin_rates, assetList))
 
+#print( "\nFin: ", fin_cer, ",", fin_var, ",", fin_sr )
+
 ## Rtail
-rtail_cer, rtail_var, rtail_corr = ExpRet_Var_and_Corr(rtail_rates, assetList)
+rtail_sr, rtail_cer, rtail_var, rtail_corr = ExpRet_Var_and_Corr(rtail_rates, assetList)
 bigList.append(ExpRet_Var_and_Corr(rtail_rates, assetList))
 
+#print( "\nRtail: ", rtail_cer, ",", rtail_var, ",", rtail_sr )
+
+
+# Question 2: Part d,e
+# Build Portfolio frontier for a portfolio
+# consisting of beer and finance
+
+beer_fin_cov = float(np.cov(beer_rates, fin_rates)[0][1])
+#print(beer_fin_cov)
+
+# Construct list of weights to construct frontier
+weights = list()
+for i in range(0,1001):
+
+    i = i/1000.0
+    temp = [i, 1.0-i]
+    weights.append(temp)
+
+mean_returns = list()
+risk = list()
+sharpe_ratio_to_weights = dict() ## Assuming they will all be unique (ehh....)
+for w in weights:
+
+    delta_beer = w[0]
+    delta_fin = w[1]
+
+    exp_portfolio_return = delta_beer*(beer_cer-0.01) + delta_fin*(fin_cer-0.01)
+    portfolio_variance = (delta_beer**2)*beer_var + (delta_fin**2)*fin_var + 2*delta_fin*delta_beer*beer_fin_cov
+
+    mean_returns.append(exp_portfolio_return)
+    risk.append(sqrt(portfolio_variance))
+
+    sharpe = (exp_portfolio_return - 0.01)/float(sqrt(portfolio_variance))
+    sharpe_ratio_to_weights[sharpe] = w
+
+
+max_sharpe_ratio = max( sharpe_ratio_to_weights.keys() )
+optimal_weighting = sharpe_ratio_to_weights[max_sharpe_ratio]
+
+print("\n Max Sharpe Ratio: ", max_sharpe_ratio)
+print("\n Optimal Weighting: ", optimal_weighting)
+
+
+plt.plot(risk, mean_returns, 'r-')
+plt.xlabel("Standard Deviation")
+plt.ylabel("Expected Returns")
+plt.title("Portfolio Frontier: Weights Distributed between Beer, Finance")
+plt.savefig("hw1_image2.png")
+plt.show()
 
 
 
-cov_matrix = list()
-cov_matrix.append(market_corr)
-cov_matrix.append(aero_corr)
-cov_matrix.append(guns_corr)
-cov_matrix.append(steel_corr)
-cov_matrix.append(ships_corr)
-cov_matrix.append(beer_corr)
-cov_matrix.append(toys_corr)
-cov_matrix.append(fin_corr)
-cov_matrix.append(rtail_corr)
 
-## print covariance matrix (even though this is truly unneeded)
-print('\n'.join([' '.join(['{:4}'.format(item) for item in row])
-      for row in cov_matrix]))
+
+
 
 
 
